175f3098cec7daf91701cb5bf8cd926022dbda9b
bushmusi <bushera.mestofa@gmail.com>
2022-05-04 16:48:57 +0300

    Insert data

--- data.sql
+++ data.sql
@@ -11,3 +11,24 @@ VALUES ('Pikachu',to_date('07 Jan 2021','DD Mon YYYY'),'15.04',FALSE,1);
 
 INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts) 
 VALUES ('Devimon',to_date('12 May 2017','DD Mon YYYY'),'11',TRUE,5); 
+
+INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts) 
+VALUES ('Charmander',to_date('08 Feb 2020', 'DD Mon YYYY'),11,FALSE,0);
+
+INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts) 
+VALUES ('Plantmon',to_date('15 Nov 2021','DD Mon YYYY'),'5.7',TRUE,2);
+
+INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts) 
+VALUES ('Squirtle',to_date('02 Jan 1993','DD Mon YYYY'),'12.13',FALSE,3);
+
+INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts) 
+VALUES ('Angemon',to_date('12 Jun 2005','DD Mon YYYY'),'15',TRUE,1); 
+
+INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts) 
+VALUES ('Boarmon',to_date('07 Jun 2005', 'DD Mon YYYY'),20.4,TRUE,7);
+
+INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts) 
+VALUES ('Blossom',to_date('13 Oct 1998','DD Mon YYYY'),'17',TRUE,3);
+
+INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts) 
+VALUES ('Ditto',to_date('14 May 2022','DD Mon YYYY'),'22',TRUE,4);
