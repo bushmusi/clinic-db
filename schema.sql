36b91e094ba970723511648efc1d838184d44934
bushmusi <bushera.mestofa@gmail.com>
2022-05-06 17:21:18 +0300

    Update the schemal

--- schema.sql
+++ schema.sql
@@ -33,3 +33,28 @@ alter table animals add constraint fk_species foreign key (species_id) reference
 
 alter table animals add column owner_id bigint;
 alter table animals add constraint fk_owner foreign key (owner_id) references owners(id) on delete cascade;
+
+create table vets(
+    id BIGSERIAL primary key ,
+    name varchar(100),
+    age int,
+    date_of_graduation date
+);
+
+create table specializations(
+	id bigserial primary key,
+    vets_id BIGSERIAL ,
+    species_id BIGSERIAL,
+    foreign key (vets_id) references vets(id) on delete cascade,
+    foreign key (species_id) references species(id) on delete cascade
+);
+
+alter table animals add constraint make_primary primary key (id);
+create table visits (
+	id bigserial primary key,
+	vets_id bigint,
+	animals_id bigint,
+	visit_date date,
+	foreign key (vets_id) references vets(id) on delete cascade,
+	foreign key (animals_id) references animals(id) on delete cascade
+);
