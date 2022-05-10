609ccf584e05c3ec9606d79ee0d1effb26891970
Jonathan Sivahera <jsivahera@icloud.com>
2022-05-10 14:33:11 +0200

    Create base tables

---
+++ schema_based_on_diagram.sql
@@ -0,0 +1,41 @@
+--create patients table
+CREATE TABLE patients(
+	id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+	name CHAR(255),
+	date_of_birth DATE
+);
+
+-- create medical_hitories table
+CREATE TABLE medical_histories(
+	id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+	admitted_at TIME,
+	patient_id INT FOREIGN KEY REFERENCES patients(id),
+	status CHAR(255)
+);
+
+-- create invoices table
+CREATE TABLE invoices(
+	id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+	total_amount DECIMAL,
+	generated_at TIME,
+	payed_at TIME,
+	medical_history_id INT FOREIGN KEY REFERENCES medical_histories(id)
+);
+
+--create invoice_items table
+CREATE TABLE invoice_items(
+	id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+	unit_price DECIMAL,
+	quantity INT,
+	total_price DECIMAL,
+	invoice_id INT FOREIGN KEY REFERENCES invoices(id),
+	treatment_id INT FOREIGN KEY REFERENCES treatments(id)
+);
+
+-- create treatments table
+CREATE TABLE treatments(
+	id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+	type CHAR(255),
+	name CHAR(255)
+);
+
